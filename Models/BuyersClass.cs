312cf9c28462a0d924c56e5a1ff851d74f790c31
enacheandrei7 <50060641+enacheandrei7@users.noreply.github.com>
2021-03-28 13:57:20 +0300

    Add files via upload

---
+++ Models/BuyersClass.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Tema_C4.Models
+{
+    public class BuyersClass
+    {
+        public int BuyerId { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public int BoughtCarId { get; set; }
+        public string Maker { get; set; }
+        public string ModelBought { get; set; }
+        public DateTime Year { get; set; }
+    }
+}
